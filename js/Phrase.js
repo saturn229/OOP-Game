c15d9e12ac348bbc2ad4828e5faa2e1e1605d0a4
saturn229 <30321956+saturn229@users.noreply.github.com>
2022-01-07 14:05:42 -0500

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -9,19 +9,35 @@
    
 
    addPhraseToDisplay(){
-      const phraseUl = document.getElementsByTagName("ul").firstElementChild;
+      const phraseUl = document.getElementById("phrase").firstElementChild;
       const randoPhrase = this.phrase.split("");
 
       console.log(randoPhrase)
       for(let i = 0; i < this.phrase.length; i++){
-        const li = document.createElement("li");
-        phraseUl.appendChild(li);
+        const phraseli = document.createElement("li");
+        if (this.phrase[i] === " "){
+          phraseli.classList.add("space");
+          phraseli.textContent = this.phrase[i];
+          phraseUl.appendChild(phraseli);
+        } else {
+          phraseli.classList.add("hide");
+          phraseli.classList.add("letter");
+          phraseli.textContent = this.phrase[i];
+          phraseUl.appendChild(phraseli);
+        }
+        
         
       }
    }
-
+   /*checks to see if the letter selected by the player matches a letter in the phrase.*/
    checkLetter(){
-
+    const keyboard = document.getElementsByClassName("key");
+    console.log(keyboard);
+    for(let i = 0; i < keyboard.length; i++){
+      console.log(keyboard[i]);
+      }
+    
+    
    }
 
    showMatchedLetter(){
