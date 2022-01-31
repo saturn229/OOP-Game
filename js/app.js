a94d6c83fe4652c198af134da81cae5a933a8398
saturn229 <30321956+saturn229@users.noreply.github.com>
2022-01-31 11:07:40 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ const game = new Game();
 
 
 game.startGame();
+
+const button = document.getElementsByClassName('key');
+
+
+for(let i = 0; i < button.length; i++){
+    button[i].addEventListener('click', event => {
+        game.handleInteraction(event.target);
+    });
+}
